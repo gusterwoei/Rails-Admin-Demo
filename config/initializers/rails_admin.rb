64aadb4d5c59f24f3b0270838bc71ed1e5b555b0
gusterwoei <gusterwoei@g-i.com.my>
2016-03-28 16:41:23 +0800

    customizing delete action

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,3 +1,5 @@
+require Rails.root.join('lib', 'rails_admin_delete.rb')
+
 RailsAdmin.config do |config|
 
     ### Popular gems integration
@@ -19,6 +21,7 @@ RailsAdmin.config do |config|
     # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
 
     ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
+
 
     config.actions do
         dashboard # mandatory
